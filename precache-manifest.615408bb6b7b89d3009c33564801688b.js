e5243f23e7a0128658b749346c8a842795a9c7a8
shadesoma <41644376+shadesoma@users.noreply.github.com>
2020-06-25 18:16:31 +0300

    Add files via upload

---
+++ precache-manifest.615408bb6b7b89d3009c33564801688b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ff2134c20a6ea0d4d4b32c6529c637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ca3ac1c2e704a8c14fa",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6580579b93ad0f06c800",
+    "url": "/static/css/main.acceb8cf.chunk.css"
+  },
+  {
+    "revision": "3ca3ac1c2e704a8c14fa",
+    "url": "/static/js/2.a2418dcf.chunk.js"
+  },
+  {
+    "revision": "4553b4dae817733db1c8eaa47a93f0de",
+    "url": "/static/js/2.a2418dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6580579b93ad0f06c800",
+    "url": "/static/js/main.83463f52.chunk.js"
+  },
+  {
+    "revision": "7d9527ba28d7cadbd57d",
+    "url": "/static/js/runtime-main.d10dd644.js"
+  }
+]);
