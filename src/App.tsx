8030134c2c38a44ea2b73d590b84ecc00d3a5031
somakun <somabaka@gmail.com>
2020-06-25 18:11:05 +0300

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {address} from '@waves/ts-lib-crypto'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [inputData, setInputData] = useState('')
+    const [cryptAddress, setCryptAddress] = useState<string>('')
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        setInputData(event.target.value)
+    }
+
+    function clickHandler(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        event.preventDefault()
+        setCryptAddress(inputData)
+        setInputData('')
+    }
+
+    return (
+        <section className="text-center">
+            <form className="form-signin">
+                {cryptAddress &&
+                <>
+                  <label htmlFor="data" className="">Address:</label>
+                  <p id="data">{cryptAddress ? address(cryptAddress) : ''}</p>
+                </>
+                }
+                <br/>
+                <h1 className="h3 mb-3 font-weight-normal">Веедите данные</h1>
+                <input className="form-control" type="text" id="input" placeholder="Введите данные"
+                       onChange={handleChange} value={inputData} name="login"/>
+                <br/>
+                <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={clickHandler}>submit
+                </button>
+                <br/>
+                <a href="https://github.com/shadesoma/ts-lib-crypto-demo">github</a>
+            </form>
+        </section>
+    );
 }
 
 export default App;
